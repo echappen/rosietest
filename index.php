fd44a0a160168fb2fbf63365084ef4514a39006d
j-beckman <beckman.joshua@gmail.com>
2013-05-03 16:41:56 -0500

    Bit better styling

--- index.php
+++ index.php
@@ -21,23 +21,15 @@
 ?>
 
 <header id="masthead" class="site-header" role="banner">
-	<div class="container">
-		
-		<div class="gravatar">
-			<?php 
-				// grab admin email and their photo
-				$admin_email = get_option('admin_email');
-				echo get_avatar( $admin_email, 100 ); 
-			?>
-		</div><!--/ author -->
-		
-		<div id="brand">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> &mdash; <span><?php echo get_bloginfo( 'description' ); ?></span></h1>
-		</div><!-- /brand -->
+	<div class="container center">
 	
 		<nav role="navigation" class="site-navigation main-navigation">
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- .site-navigation .main-navigation -->
+
+		<div id="brand">
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
+		</div><!-- /brand -->
 		
 		<div class="clear"></div>
 	</div><!--/container -->
@@ -219,9 +211,7 @@
 <footer class="site-footer" role="contentinfo">
 	<div class="site-info container">
 		<?php do_action( 'break_credits' ); ?>
-		<a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress</a>
-		<span class="sep"> and </span>
-		<a href="http://lessmade.com/themes/less" rel="theme">LESS</a> by <a href="http://jarederickson.com" rel="designer">Jared Erickson</a>
+		
 	</div><!-- .site-info -->
 </footer><!-- #colophon .site-footer -->
 
